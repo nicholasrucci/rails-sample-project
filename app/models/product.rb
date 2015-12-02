e39a91dd13bcd2fcc897529dcd188a8413f2f785
Nick <ld.tech@outlook.com>
2015-12-02 14:35:30 -0700

    validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,13 @@
 class Product < ActiveRecord::Base
+  validates :name, presence: true
+  validates :name, length: { minimum: 2 }
+  validates :name, length: { maximum: 20 }
+  validates :name, uniqueness: { case_sensitive: true }
+  validates :kind, presence: true
+  validates :kind, length: { minimum: 2 }
+  validates :kind, length: { maximum: 20 }
+  validates :cost, presence: true
+
   belongs_to :aisle
   mount_uploader :picture, PictureUploader
 end
